4e76cfc266904760745402d798e2d752af8a1c0b
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2022-11-05 10:44:38 +0100

    added some files

---
+++ IoIS_api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IoIS_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("{id:int}")]
+        public WeatherForecast Get_by_id()
+        {
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(0),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+        }
+        [HttpPost(Name = "Getaaa")]
+        public void Post()
+        {
+            Console.WriteLine("ööööö");
+            /*return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+            */
+
+        }
+    }
+}
